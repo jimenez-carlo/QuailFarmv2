2513b9430aeb857068fe9e90e96825c12a01bea3
Carlo Jimenez <jimenez.carlo.llabor@gmail.com>
2023-01-06 14:05:41 +0800

    fixed contact

---
+++ landing/contact.php
@@ -0,0 +1,38 @@
+<?php include_once('header.php'); ?>
+
+<!--  footer -->
+<footer id="contact">
+   <div class="footer">
+      <div class="container" style="height: 65vh;">
+         <div class="row">
+            <div class="col-md-4">
+               <div class="titlepage">
+                  <h2>Contact Us</h2>
+               </div>
+            </div>
+            <div class="col-md-8">
+            </div>
+            <div class="col-md-3">
+               <ul class="location_icon">
+                  <li><a href="#"><i class="fa fa-map-marker" aria-hidden="true"></i></a> Location</li>
+                  <li><a href="#"><i class="fa fa-volume-control-phone" aria-hidden="true"></i></a>09216339005</li>
+                  <li><a href="#"><i class="fa fa-envelope" aria-hidden="true"></i></a>astinpugi@gmail.com</li>
+               </ul>
+            </div>
+            <div class="col-md-9">
+               <img src="images/map.jpg" alt="" style="width: 100%;height: 160px;object-fit: cover;">
+            </div>
+         </div>
+      </div>
+      <div class="copyright">
+         <div class="container">
+            <div class="row">
+               <div class="col-md-12">
+                  <p>Copyright 2020</p>
+               </div>
+            </div>
+         </div>
+      </div>
+   </div>
+</footer>
+<?php include_once('footer.php'); ?>
